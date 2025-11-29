278b03a4641b198a10b64a5350db5c6a314eb6b1
btsai28student-lab <btsai28_student@ivycollegiate.org>
2025-11-29 03:02:53 +0000

    Created 10 properly named variables

---
+++ variable_practice.py
@@ -0,0 +1,22 @@
+# Personal Information Variables
+student_name = "Bella" #
+student_age = 16
+grade_level = 10
+
+# Academic Variables
+math_score = 95
+science_score = 90
+english_score = 95
+
+# Time & Schedule Variables
+hours_studied = 11
+days_in_week = 7
+
+# Financial Variables
+lunch_money = 300
+book_cost = 900
+
+# Print all variables to verify
+print("Student Name:", student_name)
+print("Age:", student_age)
+# Continue printing remaining variables...
