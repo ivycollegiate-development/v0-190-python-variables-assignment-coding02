eeb885379bb10538a553302cbec50a1451280b3c
Dlorcharoensin28student-spec <Dlorcharoensin28_student@ivycollegiate.org>
2025-11-29 03:12:46 +0000

    Completed independent workout tracker challenge

---
+++ variable_pratice.py
@@ -0,0 +1,22 @@
+# Personal Information Variables
+student_name = "Diamond"
+student_age = 16
+grade_level = 10
+
+# Academic Variables
+math_score = ___________
+science_score = ___________
+english_score = ___________
+
+# Time & Schedule Variables
+hours_studied = ___________
+days_in_week = ___________
+
+# Financial Variables
+lunch_money = ___________
+book_cost = ___________
+
+# Print all variables to verify
+print("Student Name:", student_name)
+print("Age:", student_age)
+# Continue printing remaining variables...
