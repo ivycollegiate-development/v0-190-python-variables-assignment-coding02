7894e338f7373db570d102712ba35dc7feb4b412
ilee28student-stack <ilee28_student@ivycollegiate.org>
2025-11-29 03:44:28 +0000

    Created 10 properly named variables

---
+++ variable-practice.py
@@ -0,0 +1,58 @@
+# P ersonal I nf ormat i on V ari ab l es
+student
+"
+"
+name =
+_
+___________
+student
+grade
+_
+age =
+___________
+level =
+_
+___________
+# A cad emi c V ari ab l es
+math
+score =
+_
+___________
+science
+score =
+_
+___________
+english
+score =
+_
+___________
+# T i me & S ch ed ul e V ari ab l es
+hours
+studied =
+days
+_
+in
+___________
+week =
+_
+_
+___________
+# F i nanci al V ari ab l es
+lunch
+_
+money =
+___________
+book
+cost =
+_
+___________
+# P ri nt al l v ari ab l es t o veri f y
+print("Student Name:"
+, student
+_
+name)
+print("Age:"
+, student
+_
+age)
+# C ont i nue pri nt i n g remai ni n g v ari ab l es...
