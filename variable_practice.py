b51f48d76522dc665c14e0453c1b1a6cfd617238
jtseng28student-blip <jtseng28_student@ivycollegiate.org>
2025-11-29 03:20:42 +0000

    Added math operations with variables

---
+++ variable_practice.py
@@ -0,0 +1,16 @@
+student_name = "jak"
+student_age = 17
+grade_level = 10
+
+
+math_score = 12
+science_score = 9
+english_score = 3
+
+hours_studied = 12837
+days_in_week = 7
+
+lunch_money = 100
+book_cost = 20
+print("Student Name:", student_name)
+print("Age:", student_age)
